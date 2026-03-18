33fdfebcba6fe642ab21908fd3269e1b6c700ba1
Rutik <rutikraundale5@gmail.com>
2026-03-18 22:15:41 +0530

    Add User Model

---
+++ backend/model/user.js
@@ -0,0 +1,77 @@
+import mongoose, { Schema } from "mongoose";
+import brcypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+
+const userschema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            index: true,
+
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+        isEmailVerified: {
+            type: Boolean,
+            default: false,
+        },
+        refreshToken: {
+            type: String,
+        },
+        forgotPasswordToken: {
+            type: String,
+        },
+        forgotPasswordExpiry: {
+            type: Date,
+        },
+        emailVerificationToken: {
+            type: String,
+        },
+        emailVerificationExpiry: {
+            type: Date,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+userschema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await brcypt.hash(this.password, 10);
+    next();
+});
+
+userschema.methods.isPasswordCorrect = async function (password) {
+    return await brcypt.compare(password, this.password);
+};
+
+userschema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+        },
+        process.env.JWT_SECRET,
+    )
+
+}
+
+
+export const User = mongoose.model("User", userschema);
