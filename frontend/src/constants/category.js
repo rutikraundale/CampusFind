6f26df4d48c1a46f654000adfe5fb70fba0f9ba6
Rutik <rutikraundale5@gmail.com>
2026-05-16 18:27:35 +0530

    Add upadated frontend

--- frontend/src/constants/category.js
+++
@@ -1,9 +0,0 @@
-export const CATEGORIES = [
-  { label: "ID Card", value: "id_card" },
-  { label: "Wallet", value: "wallet" },
-  { label: "Mobile", value: "mobile" },
-  { label: "Keys", value: "keys" },
-  { label: "Bag", value: "bag" },
-  { label: "Books", value: "books" },
-  { label: "Other", value: "other" }
-];
